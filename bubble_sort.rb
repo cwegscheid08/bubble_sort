b6b6cea66f6c6f57b9311ddf25858c890f5282ac
Chris Wegscheid <cwegscheid08@outlook.com>
2018-11-08 17:29:54 -0500

    Added random number test

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,7 @@
 def bubble_sort ary
 	bubble_sort_by = Proc.new do |x, y| 
-		case x <=> y
+		count = x <=> y
+		case count
 			when 1
 				ary[(ary.index(x)+1)] = x
 				ary[(ary.index(x))] = y
@@ -14,4 +15,12 @@ def bubble_sort ary
 	puts ary.to_s
 end
 
+
+words = ["hey", "hi", "hello"]
+num = []
+rand(1..20).times { num << rand(1..100) }
+
+
 bubble_sort([4,3,78,2,0,2])
+bubble_sort(["hey", "hi", "hello"])
+bubble_sort(num)
