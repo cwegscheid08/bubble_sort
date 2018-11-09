b1f8ff3825ee4bc6875d2c58a7869ebac06addbf
Chris Wegscheid <cwegscheid08@outlook.com>
2018-11-09 16:49:16 -0500

    Final

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,14 @@
 def bubble_sort ary
-	bubble_sort_by = Proc.new do |x, y| 
-		count = x <=> y
-		case count
-			when 1
-				ary[(ary.index(x)+1)] = x
-				ary[(ary.index(x))] = y
-			end
-		count > 0 ? y = x : y = y
-	end
-
 	(ary.length-1).times do 
-		ary.reduce(&bubble_sort_by)
+		ary.reduce do |x, y|
+			count = x <=> y
+			case count
+				when 1
+					ary[(ary.index(x)+1)] = x
+					ary[(ary.index(x))] = y
+				end
+			count > 0 ? y = x : y = y
+		end
 	end
 	puts ary.to_s
 end
@@ -24,3 +22,22 @@ rand(1..20).times { num << rand(1..100) }
 bubble_sort([4,3,78,2,0,2])
 bubble_sort(["hey", "hi", "hello"])
 bubble_sort(num)
+
+
+def bubble_sort_by ary 
+	puts ary.to_s
+	i = 0
+	unless i == (ary.length*2)-2 
+		ary.inject do |l, r| 
+			(yield(l,r) > 0 ? (ary[i], ary[i+1] = ary[i+1], ary[i]) : "")
+			i += 1 
+			r 
+		end
+	end
+	puts ary.to_s
+end
+
+
+bubble_sort_by(["hi", "hello", "hey"]) do |left,right|
+	left.length - right.length
+end
